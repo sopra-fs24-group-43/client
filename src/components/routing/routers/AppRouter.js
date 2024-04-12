75e0481dfd3185a90c0f63e00d2df62db7208e91
simonkli <simon.klipp@uzh.ch>
2024-04-12 15:33:06 +0200

    Create the Login Input fields closes #59

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -4,6 +4,7 @@ import {GameGuard} from "../routeProtectors/GameGuard";
 import GameRouter from "./GameRouter";
 import {LoginGuard} from "../routeProtectors/LoginGuard";
 import Login from "../../views/Login";
+import LoginOrRegister from "../../views/LoginOrRegister";
 
 /**
  * Main router of your application.
@@ -25,6 +26,10 @@ const AppRouter = () => {
 
         <Route path="/login" element={<LoginGuard />}>
           <Route path="/login" element={<Login/>} />
+        </Route>
+
+        <Route path="/loginOrRegister" element={<LoginGuard />}>
+          <Route path="/loginOrRegister" element={<LoginOrRegister/>} />
         </Route>
 
         <Route path="/" element={

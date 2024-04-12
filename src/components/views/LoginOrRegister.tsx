c7b4def07702ce5f6232e909e73dd2a02dbe1af7
simonkli <simon.klipp@uzh.ch>
2024-04-12 15:31:05 +0200

    Create LoginOrRegister view page closes #58

---
+++ src/components/views/LoginOrRegister.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { api, handleError } from "helpers/api";
+import User from "models/User";
+import { useNavigate } from "react-router-dom";
+import { Button } from "components/ui/Button";
+import "styles/views/Login.scss";
+import BaseContainer from "components/ui/BaseContainer";
+import PropTypes from "prop-types";
+
+const FormField = (props) => {
+  return (
+    <div className="login field">
+      <label className="login label">{props.label}</label>
+      <input
+        className="login input"
+        placeholder="enter here.."
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+FormField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState<string>(null);
+  const [username, setUsername] = useState<string>(null);
+  const [registerUsername, setRegisterUsername] = useState<string>(null);
+  const [registerName, setRegisterName] = useState<string>(null);
+
+
+  const doLogin = async () => {
+    try {
+      const requestBody = JSON.stringify({ username, name });
+      const response = await api.post("/users", requestBody);
+
+      // Get the returned user and update a new object.
+      const user = new User(response.data);
+
+      // Store the token into the local storage.
+      localStorage.setItem("token", user.token);
+
+      // Login successfully worked --> navigate to the route /game in the GameRouter
+      navigate("/game");
+    } catch (error) {
+      alert(
+        `Something went wrong during the login: \n${handleError(error)}`
+      );
+    }
+  };
+
+  return (
+    <BaseContainer>
+      <div className="login container">
+        <div className="login form">
+          <div className="login-column">
+          <h2>Login</h2>
+          <FormField
+            label="Username"
+            value={username}
+            onChange={(un: string) => setUsername(un)}
+          />
+          <FormField
+            label="Name"
+            value={name}
+            onChange={(n) => setName(n)}
+          />
+          <div className="login button-container">
+            <Button
+              disabled={!username || !name}
+              width="100%"
+              onClick={() => doLogin()}
+            >
+              Login
+            </Button>
+          </div>
+          </div>
+          <div className="login-column">
+          <h2>Register</h2>
+          <FormField
+            label="Username"
+            value={registerUsername}
+            onChange={(un: string) => setRegisterUsername(un)}
+          />
+          <FormField
+            label="Name"
+            value={registerName}
+            onChange={(n) => setRegisterName(n)}
+          />
+          <div className="login button-container">
+            <Button
+              disabled={!username || !name}
+              width="100%"
+              onClick={() => doLogin()}
+            >
+              Register
+            </Button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </BaseContainer>
+  );
+};
+
+export default Login;

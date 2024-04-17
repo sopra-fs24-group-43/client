fb5f2bbfb871954d1c58d647b63d313b75cad642
Markiian Dobosh <markiiandobosh@gmail.com>
2024-04-17 14:09:25 +0200

    Canvas on p5 API, closes #75, closes #76, closes #77

--- src/components/elements/Canvas.tsx
+++ src/components/elements/Canvas.tsx
@@ -1,32 +1,80 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import p5 from 'p5';
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLDivElement>(null);
-  
+  const [drawingTool, setDrawingTool] = useState<'pen' | 'eraser'>('pen'); 
+
   useEffect(() => {
     const canvas = new p5((p: p5) => {
-      // Setup function
-        p.setup = () => {
-          p.createCanvas(400, 400);
-          p.stroke(255); // Set line drawing color to white
-          p.strokeWeight(5); // Set line thickness
-        };
+      let drawing: any[] = []; 
 
-      // Draw function
-      p.draw = () => {
-        if (p.mouseIsPressed) {
-          p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY); // Draw a line from previous mouse position to current mouse position
+
+      p.setup = () => {
+        p.createCanvas(400, 400);
+        p.background(255);
+        p.strokeWeight(5); 
+      };
+
+      
+      const draw = () => {
+        p.stroke(0); 
+        p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY); 
+        drawing.push([p.mouseX, p.mouseY]); 
+      };
+
+
+      const erase = () => {
+        p.stroke(0); 
+        p.strokeWeight(20); 
+        p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
+        
+        for (let i = 0; i < drawing.length; i++) {
+          const [x, y] = drawing[i];
+          const distance = p.dist(x, y, p.mouseX, p.mouseY);
+          if (distance < 10) { 
+            drawing.splice(i, 1); 
+          }
         }
       };
+
+
+      p.draw = () => {
+        if (p.mouseIsPressed) {
+        
+          if (drawingTool === 'pen') {
+            draw();
+          }
+       
+          else if (drawingTool === 'eraser') {
+            erase();
+          }
+        }
+      };
+
+
+      const toggleDrawingTool = () => {
+        setDrawingTool(currentTool => currentTool === 'pen' ? 'eraser' : 'pen');
+      };
+
+
+      p.keyPressed = () => {
+        if (p.key === 'e') {
+          toggleDrawingTool();
+        }
+      };
+
+
+      setDrawingTool('pen');
+
     }, canvasRef.current!);
-  
+
     return () => {
-      canvas.remove(); // Clean up p5.js instance
+      canvas.remove(); 
     };
-  }, []);
-  
+  }, [drawingTool]);
+
   return <div ref={canvasRef}></div>;
 };
-  
+
 export default Canvas;

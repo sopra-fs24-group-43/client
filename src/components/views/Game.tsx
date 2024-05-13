d7a706dea3794f2953244b1011b62ddf09ff4edd
simonkli <160718752+simonkli@users.noreply.github.com>
2024-05-13 21:24:09 +0200

    Implementing hotkeys for canvas tools "Draw", "Fill", "Eraser", "Erase All". closes #127, #128, #129, #130.

--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -30,6 +30,33 @@ const Game = () => {
   const gameId = 1;
 
   useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "e":
+          handleEraserClick();
+          break;
+        case "c":
+          handleEraseAllClick();
+          break;
+        case "f":
+          handleFillToolClick();
+          break;
+        case "d": 
+          handleDrawToolClick();
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+  
+  useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
       navigate("/loginOrRegister");

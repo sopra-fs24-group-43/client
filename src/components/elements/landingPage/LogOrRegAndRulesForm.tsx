da36b81b1a9bd159ed261785739679961edfe737
Florian Mattmüller <florian.mattmueller@uzh.ch>
2024-05-14 17:53:01 +0200

    implement Guest in front-end mostly in Landinpage (buttons, text (whether guest or user), create guest, create inboundPlayer from guest, adding isGuest to sessionstorage), a little bit in RegisterandLogin page clsoe #43 close #39

--- src/components/elements/landingPage/LogOrRegAndRulesForm.tsx
+++ src/components/elements/landingPage/LogOrRegAndRulesForm.tsx
@@ -1,21 +1,85 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import { Button } from "components/ui/Button";
 import "styles/views/LogOrRegAndRulesForm.scss"
-
+import {Context} from "../../../context/Context";
 const LogOrRegAndRulesForm = () => {
   const navigate = useNavigate();
+  const context = useContext(Context)
+  const {stompApi, reload, setReload} = context
 
-  return (
-    <div className="LogOrRegAndRulesForm form">
-      <div className="LogOrRegAndRulesForm button-container">
+  let isGuest
+  let registered
+
+  registered = !(sessionStorage.getItem("username")=== null || sessionStorage.getItem("userId")=== null || sessionStorage.getItem("friends")=== null || sessionStorage.getItem("isGuest") === null)
+
+  isGuest = sessionStorage.getItem("isGuest")
+  const handleResponse = (payload) => {
+    var body = JSON.parse(payload.body)
+    if (body.type === "createPlayerFromGuest") {
+      sessionStorage.setItem("username", body.username);
+      sessionStorage.setItem("userId", body.userId);
+      sessionStorage.setItem("friends", "null");
+      sessionStorage.setItem("isGuest", body.isGuest);
+      isGuest = true
+      setReload(!reload)
+      stompApi.unsubscribe("/topic/landing", "LogOrRegAndRulesForm")
+    }
+  }
+  const connect =  () => {
+    stompApi.connect(() => {
+      stompApi.subscribe("/topic/landing", handleResponse, "LogOrRegAndRulesForm")
+      stompApi.connected = true
+      stompApi.send("/app/landing/createguestplayer" , "");
+    });
+  }
+  const becomeaguest = () => {
+    if (!stompApi.isConnected()) {
+      console.log("connecting to ws!")
+      connect();
+    }
+  }
+  const buttonforreg = () => {
+    if (registered) {
+      if (isGuest === "true") {
+        return "playing as guest"
+      }
+      if (isGuest === "false") {
+        return "logged in"
+      }
+    }
+    else {
+      return (
         <Button
           width="100%"
           onClick={() => navigate("/LoginOrRegister")}
         >
-            Login Or Register
+          Login Or Register
         </Button>
+      )
+    }
+  }
+  const buttonforguest = () => {
+    if (registered) {
+      return ""
+    }
+    else {
+      return (
+        <Button
+          width="100%"
+          onClick={becomeaguest}
+        >
+          Play as Guest
+        </Button>
+      )
+    }
+  }
+  return (
+    <div className="LogOrRegAndRulesForm form">
+      <div className="LogOrRegAndRulesForm button-container">
+        {buttonforreg()}
       </div>
+      {buttonforguest()}
       <div className="LogOrRegAndRulesForm img-form">
         <img src="rules.png" alt="Rule Icon" className="LogOrRegAndRulesForm img" />
       </div>
